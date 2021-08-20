22c7e789f06f14d8d67d676c5efc6eb6f9842df5
riya maria <riya.mathew@sesameindia.com>
2021-08-20 16:54:33 +0530

    API Integrated in MainActivity

--- app/src/main/java/com/example/adastraonetest/rest/ApiClient.java
+++ app/src/main/java/com/example/adastraonetest/rest/ApiClient.java
@@ -1,4 +1,36 @@
 package com.example.adastraonetest.rest;
 
+import android.content.Context;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+    static final private String BASE_URL = "http://velmm.com/apis/";
+
+    private static Retrofit retrofit = null;
+    public static Retrofit getClient(Context context) {
+        if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(logging);
+            httpClient.addNetworkInterceptor(new StethoInterceptor());
+            httpClient.readTimeout(600, TimeUnit.SECONDS);
+            httpClient.connectTimeout(600, TimeUnit.SECONDS);
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
+                    .build();
+        }
+        return retrofit;
+    }
 }
--- app/src/main/java/com/example/adastraonetest/rest/ApiInterface.java
+++ app/src/main/java/com/example/adastraonetest/rest/ApiInterface.java
@@ -1,4 +1,13 @@
 package com.example.adastraonetest.rest;
 
+import com.example.adastraonetest.model.Movie;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
+    @GET("volley_array.json")
+    Call<List<Movie>> getMovies();
 }
